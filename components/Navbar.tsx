e18fda733c0b15f8c7dafe13b1273e5985c3dc62
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-11-06 15:20:42 +0100

    Navbar design

---
+++ components/Navbar.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+export const Navbar = () => {
+    const navItems = [
+        { name: "Skills", href:"#home" },
+        { name: "Projects", href: "#features" },
+        { name: "Exp", href: "#how-it-works" },
+        { name: "Blog", href: "#pricing" },
+    ];
+
+  return (
+    <nav className='w-2xl h-15 flex items-center justify-between border border-dashed border-[#27272a]'>
+        <div className="border-r border-[#27272a] border-dashed p-3 h-full w-[30%] flex items-center">
+            <h1 className='text-zinc-300 text-xl font-doto'>0FEGZ</h1>
+        </div>
+
+        <div className="flex w-[70%] h-full justify-around items-center">
+            {navItems.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="text-zinc/80 hover:text-zinc transition-colors duration-200 text-md font-medium"
+                >
+                  /{item.name}
+                </a>
+            ))}
+        </div>
+    </nav>
+  )
+}
